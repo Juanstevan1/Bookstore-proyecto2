c11e64fcae7856b1d5e1ae230a61820fa235ca18
Ubuntu <ubuntu@ip-172-31-27-103.ec2.internal>
2025-05-13 03:22:54 +0000

    fix:web

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@bookstore-db.cr6oemqg45kn.us-east-1.rds.amazonaws.com:3306/bookstore'
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
